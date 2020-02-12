8e1181d76e0c99ac4322db25cf0700a2eb1eb481
afikmirc8 <afikmirc8@gmail.com>
2020-02-12 21:15:31 +0200

    good0

--- vol1/bs.py
+++ vol1/bs.py
@@ -2,8 +2,9 @@
 import bs4 as bs
 import urllib.request
 import sys
-reload(sys)  
-sys.setdefaultencoding('utf-8')
+from importlib import reload
+print(sys.getdefaultencoding())
+#sys.setdefaultencoding("utf-8")
 
 print(sys.argv[0:])
 sauce = urllib.request.urlopen(sys.argv[1]).read()
