f619ee07296e36f4475a6ff2758e9c1f7f925958
afikmirc8 <afikmirc8@gmail.com>
2020-02-16 20:25:52 +0200

    good3

--- start_script.sh
+++ start_script.sh
@@ -3,7 +3,7 @@ source /project_env/bin/activate
 pip3 install lxml beautifulsoup4
 export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 #fixing locale variable encoding
 
-sh ./vol/passvar.sh
+sh ./mnt/passvar.sh
 
 
 /bin/bash
