beb2e48c02f6746f8558201b03aa3f4921e95e80
fedora-user001 <fedora-user001@APC.Home>
2020-02-24 22:49:51 +0200

    goood003

--- vol1/bs-scripts/spider.py
+++ vol1/bs-scripts/spider.py
@@ -1,7 +1,6 @@
-#import bs4 as bs
-#import urllib.request
-#import sys
-#import importlib
+import bs4 as bs
+import urllib.request
+import sys
 import os
 
 script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
@@ -18,5 +17,5 @@ with open(abs_file_path) as fr:
             source = urllib.request.urlopen(line).read()
             soup = bs.BeautifulSoup(source,'lxml')
             for url in soup.find_all('a'):
-            fh.write("\n {}".format(url.get('href')))
-            f.close()
+                fh.write("\n {}".format(url.get('href')))
+                fh.close()
