e2c7aa7b24b03c5b17580db7a31cdb7b64288d74
fedora-user001 <fedora-user001@APC.Home>
2020-02-24 22:01:20 +0200

    working?

--- vol1/bs-scripts/spider.py
+++ vol1/bs-scripts/spider.py
@@ -1,16 +1,22 @@
-import bs4 as bs
-import urllib.request
-import sys
-import importlib
+#import bs4 as bs
+#import urllib.request
+#import sys
+#import importlib
+import os
 
+script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
+script_dir = os.path.split(script_path)[0] #i.e. /path/to/dir/
+rel_path = "temp/temp_urls.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
 
-print(importlib.resources.read_text(__package__, 'temp_urls.txt'))
-#fr = open(os.path.abspath('temp', 'temp_urls.txt'))
-#fh = open('/mnt/bs-scripts/temp/temp_urls.txt','a+')
-#for line in fr:
-    #print(line)
-    #source = urllib.request.urlopen(line).read()
-    #soup = bs.BeautifulSoup(source,'lxml')
-    #for url in soup.find_all('a'):
-    #fh.write("\n {}".format(url.get('href')))
-    # f.close()
+print(abs_file_path)
+
+with open(abs_file_path) as fr:
+    for line in fr:
+        print(line)
+        with open(abs_file_path, 'a+') as fh
+            source = urllib.request.urlopen(line).read()
+            soup = bs.BeautifulSoup(source,'lxml')
+            for url in soup.find_all('a'):
+            fh.write("\n {}".format(url.get('href')))
+            f.close()
