345be3cf04e7068b53c90f40b63990ebe270ed7f
afikmirc8 <afikmirc8@gmail.com>
2020-02-26 20:31:38 +0200

    first commit

--- start_script.sh
+++ start_script.sh
@@ -2,8 +2,5 @@
 source /project_env/bin/activate
 pip3 install lxml beautifulsoup4
 export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 #fixing locale variable encoding
-
-sh ./mnt/passvar.sh
-
-
 /bin/bash
+python3 /mnt/bs-scripts/spider.py
--- vol1/passvar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo this variable will pass from bash to python $URL_VAR
-python3 /mnt/bs.py $URL_VAR
