4eebb0deef1465a70a71e246f52f39bea12847c2
fedora-user001 <fedora-user001@APC.Home>
2020-02-26 05:02:11 +0200

    fix{}

--- vol1/bs-scripts/spider.py
+++ vol1/bs-scripts/spider.py
@@ -11,9 +11,14 @@ abs_file_path = os.path.join(script_dir, rel_path)
 print(abs_file_path)
 fh = open(abs_file_path, 'a+')
 with open(abs_file_path) as fr:
-    for line in fr:
+    for line in fr: #like while not EOF
         print(line)
+        if (line.startswith('https') or line.startswith('http')):
+            primary_url = line[:line.index('/')]
+        else:
+            fixed_line = '{}{}'.format(primary_url,line)
         source = urllib.request.urlopen(line).read()
+
         soup = bs.BeautifulSoup(source,'lxml')
         for url in soup.find_all('a'):
             fh.write("\n {}".format(url.get('href')))
